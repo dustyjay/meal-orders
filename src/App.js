b65bc84e1115fee8eeebe7b467b2b0aa8f08b385
dustyjay <janetarisah01@gmail.com>
2022-09-06 19:31:34 +0100

    commented out eslint error

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import UserOrders from "./UserOrders";
 function App() {
   useEffect(() => {
     fetchOrders();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const [pageLoading, setPageLoading] = useState(false);
